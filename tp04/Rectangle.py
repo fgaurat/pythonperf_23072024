bc330276c1c2ce9ca016c1e9f6603461d0bfe991
Frédéric GAURAT <fgaurat@gmail.com>
2024-07-24 10:48:31 +0200

    add getters and setters

--- tp04/Rectangle.py
+++ tp04/Rectangle.py
@@ -1,2 +1,22 @@
 class Rectangle:
-    pass
+    
+    def __init__(self,longueur,largeur):
+        print(f"def __init__(self,{longueur},{largeur})")
+        assert longueur>0 and largeur>0  
+        # if longueur<=0 or largeur<=0:
+        #     raise Exception('mauvaises valeurs pour longueur ou largeur')
+        
+        self.__longueur = longueur # _Rectangle__longueur = longueur
+        self.__largeur = largeur
+
+    def get_longueur(self):
+        return self.__longueur
+
+    def get_largeur(self):
+        return self.__largeur
+    
+    def set_longueur(self,l):
+        self.__longueur = l
+
+    def set_largeur(self,l):
+        self.__largeur = l
--- tp04/main.py
+++ tp04/main.py
@@ -3,7 +3,20 @@
 from Rectangle import Rectangle
 
 def main():
-    pass
+    r = Rectangle(3,2)
+    # print(r._Rectangle__longueur,r._Rectangle__largeur)
+    # r._Rectangle__longueur = -12
+    # print(r._Rectangle__longueur,r._Rectangle__largeur)
+
+    print(r.get_longueur()) # 3
+    print(r.get_largeur()) # 2
+
+    r.set_longueur(12)
+    print(r.get_longueur()) # 12
+
+    r.set_largeur(24)
+    print(r.get_largeur()) # 24
+
 
 if __name__ == '__main__':
     main()
